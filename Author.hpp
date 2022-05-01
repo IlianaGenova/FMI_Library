4c759abe748523310999e522130d3b40b38bea27
Iliana Genova <iliana.genova.02@gmail.com>
2022-05-01 13:19:03 +0300

    Added header file for class Author

---
+++ Author.hpp
@@ -0,0 +1,45 @@
+#ifndef __AUTHOR_HPP__
+#define __AUTHOR_HPP__
+
+using namespace std;
+#include <iostream>
+#include <string>
+
+class Author
+{
+  string name;
+  string surname;
+
+public:
+Author::Author(string name, string surname)
+{
+  this->name = name;
+  this->surname = surname;
+}
+
+string Author::getEntireName()
+{
+  return this->name + " " + this->surname;
+}
+
+string Author::getName()
+{
+  return this->name;
+}
+
+string Author::getSurname()
+{
+  return this->surname;
+}
+
+void Author::setName(string name)
+{
+  this->name = name;
+}
+
+void Author::setSurname(string surname)
+{
+  this->surname = surname;
+}
+};
+#endif
